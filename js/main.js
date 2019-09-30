139370abcf45ce74529414e744ab3741920ef41b
Thomas Leblanc <thomas.leblanc61@gmail.com>
2019-09-30 22:17:38 +0200

    Ajout de l'affichage de la ville au dessus de l'heure et de la date

--- js/main.js
+++ js/main.js
@@ -24,3 +24,26 @@ function horloge() {
     document.getElementById('horloge').innerHTML = temps;
     setTimeout(horloge, 1000);
 };
+
+// Récupération et affichage de la ville
+const callBackGetSuccess = function(data) {
+    console.log("donnees api", data);
+
+    let cityName = document.getElementById("city");
+    cityName.innerHTML = data.cities[0].name;
+}
+
+function buttonClickGET() {
+    let cityLocation = document.getElementById("cityLocation").value;
+    let urlCityLocation = "https://api.meteo-concept.com/api/location/cities?token=7a3ff296cc4f8f85d1fff02508d8202c4749a6c31016921290bb0c5bacf07027&search="+cityLocation+"";
+
+    $.get(urlCityLocation, callBackGetSuccess).done(function() {
+            //alert( "second success" );
+        })
+        .fail(function() {
+            alert( "error" );
+        })
+        .always(function() {
+            //alert( "finished" );
+        });
+};
